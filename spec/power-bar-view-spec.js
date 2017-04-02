990f81271a1aa48fef80b2ccd4b1bdf89d0afcda
Tuomas Hatakka <tuomas.hatakka@fns.fi>
2017-04-02 07:54:49 +0300

    Power bar initial

---
+++ spec/power-bar-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PowerBarView from '../lib/power-bar-view';
+
+describe('PowerBarView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
